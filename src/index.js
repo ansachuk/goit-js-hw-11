3331e2f76c28ee2881ce8abd61ca3879e8430a95
ansachuk <ansachuk@icloud.com>
2023-04-01 01:40:09 +0300

    add scream consts

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
-import axios from 'axios';
-import { Notify } from 'notiflix';
-import SimpleLightbox from 'simplelightbox/dist/simple-lightbox.esm';
-import 'simplelightbox/dist/simple-lightbox.min.css';
 import 'modern-normalize';
 import './sass/index.scss';
+
+import axios from 'axios';
+import { Notify } from 'notiflix';
+
+import SimpleLightbox from 'simplelightbox/dist/simple-lightbox.esm';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // import {} from './js/scroll';
 
 const lightbox = new SimpleLightbox('.gallery a', {
---
+++ src/js/fetch-images.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+
+const USER_KEY = '';
+const BASE_URL = 'https://pixabay.com/api/';
+const IMAGE_TYPE = 'photo';
+const ORIENTATION = 'horizontal';
+const SAFE_SEARCH = 'true';
