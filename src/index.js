d5319d590d32ac8391de084d5cc41ae14432b794
ansachuk <ansachuk@icloud.com>
2023-04-01 00:38:57 +0300

    add scroll file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { Notify } from 'notiflix';
-import SimpleLightbox from 'simplelightbox';
+import SimpleLightbox from 'simplelightbox/dist/simple-lightbox.esm';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import 'modern-normalize';
 import './sass/index.scss';
+// import {} from './js/scroll';
---
+++ src/js/scroll.js
@@ -0,0 +1,8 @@
+export const { height: cardHeight } = document
+  .querySelector('.gallery')
+  .firstElementChild.getBoundingClientRect();
+
+window.scrollBy({
+  top: cardHeight * 2,
+  behavior: 'smooth',
+});
