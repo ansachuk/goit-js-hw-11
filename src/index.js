b02df17b157e23f6d5e0da0416976b6c1a92b0eb
ansachuk <ansachuk@icloud.com>
2023-03-31 23:46:03 +0300

    change imports,add slb

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
 import axios from 'axios';
 import { Notify } from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import 'modern-normalize';
+import './sass/index.scss';
