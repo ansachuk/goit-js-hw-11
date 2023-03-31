a09ef4de102e66754d05069d0350d12d869ea4b7
ansachuk <ansachuk@icloud.com>
2023-04-01 01:06:08 +0300

    add base styles and js

--- src/index.js
+++ src/index.js
@@ -5,3 +5,10 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 import 'modern-normalize';
 import './sass/index.scss';
 // import {} from './js/scroll';
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  /* options */
+});
+
+const searchFormRef = document.querySelector('#search-form');
+const galleryWrapperRef = document.querySelector('.gallery');
